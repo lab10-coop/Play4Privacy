c2d2e4199d0d771752eab3817c45c477f420c58a
davidf <dforsten@gmail.com>
2017-07-25 17:36:15 +0200

    * Made board CSS work within a bootstrap column.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css'
 
 class Square extends React.Component {
   // Expects two props to be sent:
