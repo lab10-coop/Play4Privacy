69c23eb3759bde34207fb9c327d3b56b0747ffcc
davidf <dforsten@gmail.com>
2017-08-13 20:11:19 +0200

    Converted credits.html to Credits.js Moved NavBar into index.js to have all code relevant to routing and navigation in one place

--- frontend/src/Credits.js
+++ frontend/src/Credits.js
@@ -21,6 +21,49 @@ function Credits() {
             takimata sanctus est Lorem ipsum dolor sit amet.</p>
         </div>
       </div>
+      <div className='field' id='ethYesField'>
+        <div className='fieldInner'>
+          <h2>Get your credits</h2>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+            vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+          <div className='walletQR'>HIER KOMMT DER WALLET-QR-CODE REIN</div>
+          <p>
+            <span className='ethWalletAdress'>xyzxyzxyzxyzxyzxyzxyzxyzxy</span>
+            <span className='button' id='ethWalletNext'>NEXT</span>
+          </p>
+        </div>
+      </div>
+      <div className='field' id='ethNoField'>
+        <div className='fieldInner'>
+          <h2>Get your credits</h2>
+          <p>Instruction on how to get credits... Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+            aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+            rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
+            sit amet.</p>
+          <div className='freeGames clear'>
+            <span className='freeGamesLeft'>133</span>
+            <span> free games remaining in this batch</span>
+            <span className='freeGamesBar' />
+          </div>
+
+          <div className='claimFreeGame'>
+            <h2>claim a free game</h2>
+            <p>Just answer the question below and claim your credit</p>
+            <p>
+              <span className='freeGameQuestion'>
+                What is the name of the exhibition this game is about?
+              </span>
+            </p>
+            <form>
+              <input type='text' className='text' placeholder='Enter your answer here...' />
+              <input type='submit' className='submit' value='send' />
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import NavBar from './NavBar';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Home from './Home';
 import Credits from './Credits';
 import './css/styles-responsive.css';
 import './css/styles.css';
 
+function Navigation() {
+  return (
+    <header>
+      <div className='inner'>
+        <div className='logo'>Play 4 Privacy</div>
+        <nav>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/credits'>Credits</Link></li>
+            <li><Link to='/gameboard'>Gameboard</Link></li>
+            <li><Link to='/leaderboard'>Leaders</Link></li>
+            <li><Link to='/endgame'>Endgame</Link></li>
+          </ul>
+        </nav>
+        <div className='clear' />
+      </div>
+    </header>
+  );
+}
+
 function Routes() {
   return (
     <div>
-      <NavBar />
+      <Navigation />
       <Switch>
         <Route path='/credits' component={Credits} />
         <Route path='/' component={Home} />
