96dd4bf6a6f43037d88f2433373f1e12793063a5
davidf <dforsten@gmail.com>
2017-08-13 14:04:01 +0200

    Migrated all body content of index.html to jsx Removed "no-hash" rule - annoying and removed from the newest version of jsx-a11y anyways

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,9 +10,68 @@ function Game() {
   );
 }
 
+function IndexPage() {
+  return (
+    <div>
+      <header>
+        <div className='inner'>
+          <div className='logo'>Play 4 Privacy</div>
+          <nav>
+            <ul>
+              <li><a href='index.html'>Home</a></li>
+              <li><a href='credits.html'>Credits</a></li>
+              <li><a href='gameboard.html'>Gameboard</a></li>
+              <li><a href='leaderboard.html'>Leaders</a></li>
+              <li><a href='endgame.html'>Endgame</a></li>
+            </ul>
+          </nav>
+          <div className='clear' />
+        </div>
+      </header>
+
+      <div className='projectExplanation field'>
+        <h2>How it works, and wtf is this all about?</h2>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+          tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
+          eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet.</p>
+      </div>
+
+      <div className='creditsButton field'>
+        <a className='button' href='credits.html' title='Get Credits'>Get Credits</a>
+      </div>
+
+      <div className='currentGame field'>
+        <h2>Current Game</h2>
+
+        <div className='liveCam'>
+          <img src='http://bixcam.kunsthausgraz.at/out/stream/webcam2_x.jpg' alt='bix Livecam' />
+        </div>
+
+        <div className='gameField'>
+          <Game />
+        </div>
+
+        <div className='playTime clear'>
+          Playtime: <span className='timeLeft'>7min</span> left of
+          <span className='totalTime'>10min</span>
+          <span className='timeBar' />
+        </div>
+      </div>
+      <div className='partners field'>
+        <h2>Partners</h2>
+        <a href='#'>Partner #1</a>
+        <a href='#'>Partner #2</a>
+        <a href='#'>Partner #3</a>
+        <a href='#'>Partner #4</a>
+      </div>
+    </div>
+  );
+}
+
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <IndexPage />,
   document.getElementById('react_root'),
 );
