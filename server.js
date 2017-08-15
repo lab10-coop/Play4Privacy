0424a62d2a143d023fea841acfd28f217c7d623a
davidf <dforsten@gmail.com>
2017-08-15 23:48:05 +0200

    Added a server-side catch-all route just serving index.html to mirror client-side routing for everything except  API calls

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -8,6 +9,12 @@ app.set('port', process.env.PORT || 3001);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
 }
+
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, 'frontend/build/index.html'));
+});
 
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
