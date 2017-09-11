b716221b5df52605de39d130eea50679b7350914
davidf <dforsten@gmail.com>
2017-09-11 05:21:30 +0200

    Added JQuery support to Home.js Converted Home component from a function component to a class component and added the "componentDidMount" member function as hook for JQuery code

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,50 +1,64 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react';
+import $ from 'jquery';
 import Board from '../Board';
 
-const Home = inject('game')(observer((props) => {
-  const timeBarStyles = {
-    width: props.game.percentageLeftinGame,
-  };
-  return (
-    <div>
-      <div className='projectExplanation field'>
-        <h2>How it works, and wtf is this all about?</h2>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-          tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
-          eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-          takimata sanctus est Lorem ipsum dolor sit amet.</p>
-      </div>
+@inject('game')
+@observer
+class Home extends React.Component {
+  componentDidMount() {
+  }
 
-      <div className='creditsButton field'>
-        <a className='button' href='credits' title='Get Credits'>Click here to get Credits</a>
-      </div>
-
-      <div className='currentGame field'>
-        <h2>Current Game</h2>
-
-        <div className='liveCam'>
-          <figure>
-            <img src='http://bixcam.kunsthausgraz.at/out/stream/webcam2_x.jpg' alt='bix Livecam' />
-            <figcaption>Current Live-Feed from BixCam </figcaption>
-          </figure>
+  render() {
+    const timeBarStyles = {
+      width: this.props.game.percentageLeftinGame,
+    };
+    return (
+      <div>
+        <div className='projectExplanation field'>
+          <h2>How it works, and wtf is this all about?</h2>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
+            eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+            takimata sanctus est Lorem ipsum dolor sit amet.</p>
         </div>
 
-        <div className='gameField'>
-          <Board />
+        <div className='creditsButton field'>
+          <a className='button' href='credits' title='Get Credits'>Click here to get Credits</a>
         </div>
 
-        <div className='playTime clear'>
-          Playtime: <span className='timeLeft'>{props.game.timeLeftInGame.getMinutes()}m&nbsp;
-            {props.game.timeLeftInGame.getSeconds()}s</span> left of
-          <span className='totalTime'> {props.game.maxGameDuration.getMinutes()}m</span>
-          <span className='timeBar' style={timeBarStyles} />
+        <div className='currentGame field'>
+          <h2>Current Game</h2>
+
+          <div className='liveCam'>
+            <figure>
+              <img
+                src='http://bixcam.kunsthausgraz.at/out/stream/webcam2_x.jpg'
+                alt='bix Livecam'
+              />
+              <figcaption>Current Live-Feed from BixCam </figcaption>
+            </figure>
+          </div>
+
+          <div className='gameField'>
+            <Board />
+          </div>
+
+          <div className='playTime clear'>
+            Playtime:&nbsp;
+            <span className='timeLeft'>
+              {this.props.game.timeLeftInGame.getMinutes()}m&nbsp;
+              {this.props.game.timeLeftInGame.getSeconds()}s
+            </span>
+            &nbsp;left of
+            <span className='totalTime'> {this.props.game.maxGameDuration.getMinutes()}m</span>
+            <span className='timeBar' style={timeBarStyles} />
+          </div>
         </div>
+
       </div>
-
-    </div>
-  );
-},
-));
+    );
+  }
+}
 
 export default Home;
