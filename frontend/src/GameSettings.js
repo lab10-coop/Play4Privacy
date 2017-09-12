e676864238ed054a859affdec5b3bf9ede667e90
davidf <dforsten@gmail.com>
2017-09-12 20:26:25 +0200

    Using old-school anonymous functions in GameSettings.js which is used directly, without babel compile, in the production builds.

--- frontend/src/GameSettings.js
+++ frontend/src/GameSettings.js
@@ -1,3 +1,4 @@
+/* eslint func-names: ["error", "never"] */
 function GameSettings() {
   this.BOARD_SIZE = 9;
   this.BOARD_SIZE_SQUARED = this.BOARD_SIZE * this.BOARD_SIZE;
@@ -23,7 +24,7 @@ function GameSettings() {
   this.ERROR_ALREADY_OCCUPIED = 2;
   this.ERROR_SUICIDE = 3;
 
-  this.idxToCoord = (idx) => {
+  this.idxToCoord = function (idx) {
     let column = idx % this.BOARD_SIZE;
     const row = this.BOARD_SIZE - Math.floor(idx / this.BOARD_SIZE);
     // Special case, we skip 'I' and continue with 'J' instead
@@ -33,7 +34,7 @@ function GameSettings() {
     return String.fromCharCode('A'.charCodeAt(0) + column) + row.toString();
   };
 
-  this.teamToString = (team) => {
+  this.teamToString = function (team) {
     if (team === this.BLACK) {
       return 'BLACK';
     } else if (team === this.WHITE) {
