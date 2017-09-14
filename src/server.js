6d53bafaf08562508057156a7027827bb2ae8847
Oskar Bechtold <bechtold@brightsolutions.de>
2017-09-14 19:22:36 +0200

    Delinted.

--- src/server.js
+++ src/server.js
@@ -9,6 +9,8 @@ const app = express();
 const server = http.Server(app);
 const io = socketio(server);
 
+const game = new Game(io);
+
 app.set('port', process.env.PORT || 3001);
 
 // Express only serves static assets in production
@@ -19,11 +21,11 @@ if (process.env.NODE_ENV === 'production') {
 // REST endpoint serving Board in JSON for Processing
 app.get('/board', (request, response) => {
   const res = {
-    "time" : Date.now(),
-    "startTime" : game.startTime,
-    "currentTeam": game.go.currentTeam(),
-    "board": game.go.board,
-    "gameState": game.gameState
+    time: Date.now(),
+    startTime: game.startTime,
+    currentTeam: game.go.currentTeam(),
+    board: game.go.board,
+    gameState: game.gameState,
   };
   response.send(JSON.stringify(res));
 });
@@ -34,7 +36,6 @@ app.get('*', (request, response) => {
   response.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
 
-const game = new Game(io);
 
 io.on('connection', (socket) => {
   console.log('a user connected');
