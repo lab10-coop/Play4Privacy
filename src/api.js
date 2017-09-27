db5e2f30cb265bed6e9a80103ea4cd98ceea716a
davidf <dforsten@gmail.com>
2017-09-27 13:26:40 +0200

    Protected relevant server-side API handlers against malformed API calls crashing the server Implemented a "checkedCall" function for wrapping API function argument calls with try/catch blocks

--- src/api.js
+++ src/api.js
@@ -1,16 +1,28 @@
+function checkedCall(fn) {
+  try {
+    fn();
+  } catch (err) {
+    console.log(`API call error!! Error message: ${err.message}`);
+  }
+}
+
 export function defineClientApi(game, socket) {
   // Emit this event to receive the current game state from cratch,
   // for example right after connecting/reconnecting to the server.
   socket.on('current game state', (id, timestamp, fn) => {
-    fn(timestamp, Date.now() - game.startTime, game.go.currentTeam(), game.hasJoined(id),
-      game.playerMove(id), game.go.board, game.gameState);
+    checkedCall(() => {
+      fn(timestamp, Date.now() - game.startTime, game.go.currentTeam(), game.hasJoined(id),
+        game.playerMove(id), game.go.board, game.gameState);
+    });
   });
 
   // Emit this event to join the currently running game,
   // returns the team you are assigned to, or an empty string
   // if joining failed.
-  socket.on('join game', (id, fn) => fn(game.joinGame(id)));
-  socket.on('submit move', (id, move, sig, fn) => fn(game.submitMove(id, move, sig)));
+  socket.on('join game', (id, fn) => checkedCall(() => fn(game.joinGame(id))));
+  socket.on('submit move', (id, move, sig, fn) =>
+    checkedCall(() => fn(game.submitMove(id, move, sig))),
+  );
 
   socket.on('redeem tokens', (id) => { console.log("TODO: implement 'redeem tokens'"); });
   socket.on('donate tokens', (id) => { console.log("TODO: implement 'donate tokens'"); });
